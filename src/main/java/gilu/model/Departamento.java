3c5738261ac79dbcfbf38f379b459e3d455588bc
Gisele Larissa <giselealves016@gmail.com>
2018-08-27 14:15:04 -0300

    mudança nos atrributos da classe Departamentos

--- src/main/java/gilu/model/Departamento.java
+++ src/main/java/gilu/model/Departamento.java
@@ -14,12 +14,11 @@ public class Departamento {
 	@GeneratedValue
 	private Integer id;
 	
-	private String eletronico;
-	private String livros; 
-	private String ferramentas;
-	private String videoGames; 
-	private String acessorios;
-	private String pepelaria;
+	
+	
+	private String infantil;
+	private String femenino; 
+	private String masculino;
 	
 	
 	public Departamento(){
@@ -27,15 +26,12 @@ public class Departamento {
 	}
 
 
-	public Departamento(String eletronico, String livros, String ferramentas, String videoGames, String acessorios,
-			String pepelaria) {
+	public Departamento(Integer id, String infantil, String femenino, String masculino) {
 		super();
-		this.eletronico = eletronico;
-		this.livros = livros;
-		this.ferramentas = ferramentas;
-		this.videoGames = videoGames;
-		this.acessorios = acessorios;
-		this.pepelaria = pepelaria;
+		this.id = id;
+		this.infantil = infantil;
+		this.femenino = femenino;
+		this.masculino = masculino;
 	}
 
 
@@ -49,63 +45,33 @@ public class Departamento {
 	}
 
 
-	public String getEletronico() {
-		return eletronico;
+	public String getInfantil() {
+		return infantil;
 	}
 
 
-	public void setEletronico(String eletronico) {
-		this.eletronico = eletronico;
+	public void setInfantil(String infantil) {
+		this.infantil = infantil;
 	}
 
 
-	public String getLivros() {
-		return livros;
+	public String getFemenino() {
+		return femenino;
 	}
 
 
-	public void setLivros(String livros) {
-		this.livros = livros;
+	public void setFemenino(String femenino) {
+		this.femenino = femenino;
 	}
 
 
-	public String getFerramentas() {
-		return ferramentas;
+	public String getMasculino() {
+		return masculino;
 	}
 
 
-	public void setFerramentas(String ferramentas) {
-		this.ferramentas = ferramentas;
-	}
-
-
-	public String getVideoGames() {
-		return videoGames;
-	}
-
-
-	public void setVideoGames(String videoGames) {
-		this.videoGames = videoGames;
-	}
-
-
-	public String getAcessorios() {
-		return acessorios;
-	}
-
-
-	public void setAcessorios(String acessorios) {
-		this.acessorios = acessorios;
-	}
-
-
-	public String getPepelaria() {
-		return pepelaria;
-	}
-
-
-	public void setPepelaria(String pepelaria) {
-		this.pepelaria = pepelaria;
+	public void setMasculino(String masculino) {
+		this.masculino = masculino;
 	}
 
 
@@ -113,13 +79,10 @@ public class Departamento {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((acessorios == null) ? 0 : acessorios.hashCode());
-		result = prime * result + ((eletronico == null) ? 0 : eletronico.hashCode());
-		result = prime * result + ((ferramentas == null) ? 0 : ferramentas.hashCode());
+		result = prime * result + ((femenino == null) ? 0 : femenino.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((livros == null) ? 0 : livros.hashCode());
-		result = prime * result + ((pepelaria == null) ? 0 : pepelaria.hashCode());
-		result = prime * result + ((videoGames == null) ? 0 : videoGames.hashCode());
+		result = prime * result + ((infantil == null) ? 0 : infantil.hashCode());
+		result = prime * result + ((masculino == null) ? 0 : masculino.hashCode());
 		return result;
 	}
 
@@ -133,41 +96,28 @@ public class Departamento {
 		if (getClass() != obj.getClass())
 			return false;
 		Departamento other = (Departamento) obj;
-		if (acessorios == null) {
-			if (other.acessorios != null)
+		if (femenino == null) {
+			if (other.femenino != null)
 				return false;
-		} else if (!acessorios.equals(other.acessorios))
-			return false;
-		if (eletronico == null) {
-			if (other.eletronico != null)
-				return false;
-		} else if (!eletronico.equals(other.eletronico))
-			return false;
-		if (ferramentas == null) {
-			if (other.ferramentas != null)
-				return false;
-		} else if (!ferramentas.equals(other.ferramentas))
+		} else if (!femenino.equals(other.femenino))
 			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (livros == null) {
-			if (other.livros != null)
+		if (infantil == null) {
+			if (other.infantil != null)
 				return false;
-		} else if (!livros.equals(other.livros))
+		} else if (!infantil.equals(other.infantil))
 			return false;
-		if (pepelaria == null) {
-			if (other.pepelaria != null)
+		if (masculino == null) {
+			if (other.masculino != null)
 				return false;
-		} else if (!pepelaria.equals(other.pepelaria))
-			return false;
-		if (videoGames == null) {
-			if (other.videoGames != null)
-				return false;
-		} else if (!videoGames.equals(other.videoGames))
+		} else if (!masculino.equals(other.masculino))
 			return false;
 		return true;
 	}
+
+
 }
